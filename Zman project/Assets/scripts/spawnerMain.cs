8b78af0af00a02f3a4507a25a6d7938855aeecc5
zach-rgb <zjose1@ocdsb.ca>
2025-01-17 12:24:09 -0500

    Update spawnerMain.cs

--- Zman project/Assets/scripts/spawnerMain.cs
+++ Zman project/Assets/scripts/spawnerMain.cs
@@ -11,42 +11,60 @@ public class spawnerMain : MonoBehaviour
     private bool spawn=false;
     [SerializeField] private string tag;
     public storage storage;
+    private int mode;
+    public List<(float, GameObject)> Targetdistances = new List<(float, GameObject)>();
+    private float time_elasped
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        
+        if (this.tag=="player"){
+            mode=1
+        }
+        else if(this.tag=="AI"){
+            mode=2
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        time_elasped += Time.deltaTime;
+        
         
             
-            if (target_finding.findingDistanceTag(tag) < max_distance)
-            {
-                spawn = true;
-            }
-        
-        
-        time_elasped += Time.deltaTime;
+            Targetdistances.Clear();
 
-        if (Mathf.FloorToInt(time_elasped % 60) >= time)
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
+
+        foreach (GameObject target in targets)
+        {
+            Targetdistances.Add((Vector3.Distance(target.transform.position, transform.position), target));
+        }
+        Targetdistances.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+        if (Targetdistances[0].Item1 < max_distance){
+            spawn=true
+        }
+        
+        
+    
+
+
+    if (mode==1){
+        
+        if ((time_elasped % 60) >= max_time && spawn && ObjectToSpawn.GetComponent<cost>().CostOfObject<storage.player_metal)
         {
             time_elasped = 0;
-            return true;
+            Instantiate(ObjectToSpawn, transform.position, transform.rotation, transform);
         }
+    }
+    else if(mode==2){
+        if ((time_elasped % 60) >= max_time && spawn && ObjectToSpawn.GetComponent<cost>().CostOfObject<storage.AI_metal)
+        {
+            time_elasped = 0;
+            Instantiate(ObjectToSpawn, transform.position, transform.rotation, transform);
+        }
+    }
         
-        if ((time_elasped % 60) >= time && ObjectToSpawn.GetComponent<cost>().CostOfObject<storage.)
-        {
-            Instantiate(ObjectToSpawn, position, rotation, ParentObject.transform);
-        }
-        if (spawn)
-        {
-            Debug.Log("hallo");
-            spawning.spawn(max_time, ObjectToSpawn.GetComponent<cost>().CostOfObject, ObjectToSpawn, transform.position, transform.rotation, this.gameObject);
-        }
     }
 }
 
